2fa6df1a1bd4e9e3cfc5a531d6fe8074380e3649
Jun Kato <i@junkato.jp>
2018-01-30 04:43:11 +0900

    Add English page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
 const packageJson = require('./package.json');
 
@@ -8,20 +9,29 @@ const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('semantic', require('./semantic/tasks/build'));
 
-gulp.task('pug', function(){
-  return gulp.src('src/**/*.pug')
-    .pipe(pug({
-      locals: {
-          package: packageJson
-        , taDomain: 'textalive.jp'
-        , ja: true
-        , songs: require('./songs.json')
-      },
-      verbose: true,
-      pretty: true
-    }))
-    .pipe(gulp.dest('docs/'));
-});
+gulp.task('pug', ['pug:ja', 'pug:en']);
+
+function compilePug(lang) {
+  const l = lang;
+  return function(){
+      gulp.src('src/index.pug')
+        .pipe(pug({
+          locals: {
+              package: packageJson
+            , taDomain: 'textalive.jp'
+            , ja: l === 'ja'
+            , songs: require('./songs.json')
+          },
+          verbose: true,
+          pretty: true
+        }))
+        .pipe(rename({ suffix: l === 'ja' ? '' : '.en' }))
+        .pipe(gulp.dest('docs'));
+    };
+}
+
+gulp.task('pug:ja', compilePug('ja'));
+gulp.task('pug:en', compilePug('en'));
 
 gulp.task('ts', function(){
   return gulp.src('src/typescripts/**/*.ts')
@@ -37,7 +47,7 @@ gulp.task('browser-sync', function(){
       }
     });
     gulp.watch('docs/**/*.{html,js}', ['reload']);
-    gulp.watch('src/**/*.pug', ['pug']);
+    gulp.watch('src/index.pug', ['pug']);
     gulp.watch('src/typescripts/**/*.ts', ['ts']);
   });
   
